352e2794fc73f5934aacfff71bab05d984d60f12
labit-riset-umm <71822965+alifftwa@users.noreply.github.com>
2023-11-07 21:35:31 +0700

    first-commit

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../../components/fragments/Navbar/Navbar";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>Section 1</div>
+      <div>Section 2</div>
+      <div>Section 3</div>
+      <div>Section 4</div>
+      <div>Footer</div>
+    </div>
+  );
+};
+
+export default LandingPage;
