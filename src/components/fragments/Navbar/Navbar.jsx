b8d589385ae210e54f7e19544be97f4079945d0f
labit-riset-umm <71822965+alifftwa@users.noreply.github.com>
2023-11-08 18:26:36 +0700

    update navbar

--- src/components/fragments/Navbar/Navbar.jsx
+++ src/components/fragments/Navbar/Navbar.jsx
@@ -1,9 +1,64 @@
 import React from "react";
+import logoEmpathiCare from "../../../assets/logoEmpathiCare.png";
+import logoKemenkes from "../../../assets/logokemenkes.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>Navbar</div>
+    <div class='d-flex justify-content-center' >
+      <nav class="navbar navbar-expand-lg bg-transparent">
+        <div class="container-fluid gap-0 column-gap-4 d-flex justify-content-center ">
+          <div>
+            <a class="navbar-brand d-flex p-2 g-col-6 column-gap-4 " href="#">
+              <img src={logoEmpathiCare} alt="" />
+              <div class="vr"></div>
+              <img src={logoKemenkes} alt="" />
+            </a>
+          </div>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div
+            class="collapse navbar-collapse p-2 g-col-6"
+            id="navbarSupportedContent"
+          >
+            <ul class="navbar-nav mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Service
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About Us
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Register
+                </a>
+              </li>
+            </ul>
+
+            <button class="rounded-pill mx-5 bg-transparent border-primary">
+              <a class="nav-link my-2 mx-2 text-primary">Download Sekarang</a>
+            </button>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Navbar from "../../components/fragments/Navbar/Navbar";
+import Layout from "../../layout";
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
-      <div>Section 1</div>
-      <div>Section 2</div>
-      <div>Section 3</div>
-      <div>Section 4</div>
-      <div>Footer</div>
+      <Layout>
+        <div>Section 1</div>
+        <div>Section 2</div>
+        <div>Section 3</div>
+        <div>Section 4</div>
+        <div>Footer</div>
+      </Layout>
     </div>
   );
 };
