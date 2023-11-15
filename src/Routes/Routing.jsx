2fc42d8281658b6217a4b4cca44a45e7933e23e0
karimDev <kromosom06@gmail.com>
2023-11-15 19:01:33 +0700

    update: Routing

--- src/Routes/Routing.jsx
+++ src/Routes/Routing.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Dashboard from '../pages/Dashboard'
-import Chat from '../pages/Chat'
-import Transaksi from '../pages/Transaksi'
-import Artikel from '../pages/Artikel'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import Chat from "../pages/Chat";
+import Transaksi from "../pages/Transaksi";
+import Artikel from "../pages/Artikel";
+import LandingPage from "../pages/LandingPage/LandingPage";
 
 const Routing = () => {
   return (
     <Routes>
-      <Route path='/' element={<Dashboard/>} />
-      <Route path='/dokter-chat' element={<Chat/>} />
-      <Route path='/dokter-transaksi' element={<Transaksi/>} />
-      <Route path='/dokter-artikel' element={<Artikel/>} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dokter-chat" element={<Chat />} />
+      <Route path="/dokter-transaksi" element={<Transaksi />} />
+      <Route path="/dokter-artikel" element={<Artikel />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default Routing
+export default Routing;
--- src/components/fragments/Sidebar/Sidebar.jsx
+++ src/components/fragments/Sidebar/Sidebar.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import {
   BsChatLeft,
@@ -7,10 +6,9 @@ import {
   BsBoxArrowRight,
   BsFileEarmarkText,
 } from "react-icons/bs";
-import './Sidebar.styles.css'
+import "./Sidebar.styles.css";
 
 import SidebarItem from "../../elements/SidebarItem/SidebarItem";
-
 
 const Sidebar = () => {
   return (
@@ -28,7 +26,7 @@ const Sidebar = () => {
           <div>
             <img id="logo" src="assets/logo.png" alt="logo" />
             <SidebarItem
-              path={"/"}
+              path={"/dashboard"}
               id={"dashboard-link"}
               text="Dashboard"
               svg={<BsGrid size={25} strokeWidth={0.5} />}
@@ -37,23 +35,23 @@ const Sidebar = () => {
               path={"/dokter-chat"}
               id="chat-link"
               text="Chat"
-              svg={<BsChatLeft size={25} strokeWidth={0.5}  />}
+              svg={<BsChatLeft size={25} strokeWidth={0.5} />}
             />
             <SidebarItem
               path={"/dokter-transaksi"}
               id={"transaksi-link"}
               text="Transaksi"
-              svg={<BsWallet2 size={25} strokeWidth={0.5}  />}
+              svg={<BsWallet2 size={25} strokeWidth={0.5} />}
             />
             <SidebarItem
               path={"/dokter-artikel"}
               id={"artikel-link"}
               text="Artikel"
-              svg={<BsFileEarmarkText size={25} strokeWidth={0.5}  />}
+              svg={<BsFileEarmarkText size={25} strokeWidth={0.5} />}
             />
             <SidebarItem
               path={"/dokter-logout"}
-              style={{marginTop:" 396px"}}
+              style={{ marginTop: " 396px" }}
               className="logout-link"
               id="logout-link"
               text="Logout"
@@ -65,6 +63,5 @@ const Sidebar = () => {
     </div>
   );
 };
-
 
 export default Sidebar;
