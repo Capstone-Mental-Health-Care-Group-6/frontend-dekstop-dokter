a638b8839364f4f3824190408ef87f7ad643f2e1
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-11-28 16:26:45 +0700

    membuat petunjuk axios interceptors

--- src/service/getAllDoctors.js
+++ src/service/getAllDoctors.js
@@ -1,5 +1,6 @@
 // ini hanya sintak perumpamaan nya saja ya, nanti samakan lagi sesuai apa yang di BE
 // sintak ini hanya contoh boleh di hapus atau di pakai dan ubah
+// tidak menggunakan url dari env karena sudah di setting di axiosInterceptor jadi tinggal endpoint nya saja
 
 
 import { axiosInterceptor } from "./axiosInterceptors";
