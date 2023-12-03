3cba41a7cb42a5eb8aade4b59bdbbf5782514cc7
karimDev <kromosom06@gmail.com>
2023-12-03 14:33:17 +0700

    add: page not found 4040

--- image.js
+++ image.js
@@ -43,6 +43,7 @@ import avatar2 from "./src/assets/avatar2.svg";
 import avatar3 from "./src/assets/avatar3.svg";
 import avatar4 from "./src/assets/avatar4.svg";
 import imgModalSaldoCair from "./src/assets/imgModalSaldoCair.png";
+import imgNotFound404 from "./src/assets/imgNotFound404.png";
 
 export {
   logoEmpathiCareMobile,
@@ -90,4 +91,5 @@ export {
   sendChat,
   logoZoom,
   imgModalSaldoCair,
+  imgNotFound404,
 };
