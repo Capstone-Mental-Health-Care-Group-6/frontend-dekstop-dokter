ae93bd7e03dd11df95601f0ef7293a974cb15d2b
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-12-05 10:40:56 +0700

    slicing ui

--- src/pages/ChatBot/ChatBot.jsx
+++ src/pages/ChatBot/ChatBot.jsx
@@ -57,56 +57,6 @@ const ChatBot = () => {
     setFormMessage({ message: "" });
   };
 
-  const handlePromptClick = (prompt) => {
-    const doctorPrompt = {
-      sender: "dokter",
-      content: prompt,
-    };
-    setDataChat([...dataChat, doctorPrompt]);
-    setSelectedPrompt(prompt.toLowerCase());
-    // Check if the prompt is one of the predefined options
-    if (
-      [
-        "mengatasi gangguan kecemasan",
-        "mengatasi stress",
-        "mengatasi depresi",
-        "mengatasi kegilaan",
-      ].includes(prompt.toLowerCase())
-    ) {
-      // Handle predefined prompts
-      handlePredefinedPrompt(prompt.toLowerCase());
-    } else {
-      // Handle other prompts
-      handleOpenEndedPrompt(prompt);
-    }
-  };
-
-  const handlePredefinedPrompt = (prompt) => {
-    // Implement your logic for predefined prompts
-    // Example: Show specific responses based on the chosen prompt
-    switch (prompt) {
-      case "mengatasi kegilaan":
-        const response1 =
-          "Dukungan keluarga dan teman-teman sangat penting. Membangun jaringan dukungan yang kuat dapat membantu individu dengan gangguan kecemasan merasa lebih terhubung dan kurang terisolasi. Apakah masalah Anda sudah terselesaikan?";
-        setDataChat([...dataChat, { sender: "bot", content: response1 }]);
-        break;
-      case "mengatasi stress":
-        const response2 =
-          "Pernahkah Anda mencoba teknik relaksasi seperti meditasi atau pernapasan dalam? Aktivitas fisik yang teratur juga dapat membantu mengurangi stres.";
-        setDataChat([...dataChat, { sender: "bot", content: response2 }]);
-        break;
-      // Add responses for other predefined prompts
-      default:
-        console.log("Prompt not recognized:", prompt);
-    }
-  };
-
-  const handleOpenEndedPrompt = (prompt) => {
-    // Implement your logic for open-ended prompts
-    // Example: Ask clarifying questions or provide resources
-    const response = `Terima kasih atas pesan Anda: ${prompt}. Mohon maaf, saya masih dalam pengembangan dan belum bisa memahami semua jenis pertanyaan. Apakah Anda bisa memberikan informasi lebih lanjut tentang masalah Anda?`;
-    setDataChat([...dataChat, { sender: "bot", content: response }]);
-  };
 
   const handleFinishChat = (status) => {
     setIsFinished(true);
