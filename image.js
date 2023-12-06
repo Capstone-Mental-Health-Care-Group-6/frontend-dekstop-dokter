f80bccee004a302d4380f0182a8c814f9310fafb
nomccc <nuravivah224@gmail.com>
2023-12-06 13:54:51 +0800

    update: halaman artikel

--- image.js
+++ image.js
@@ -46,7 +46,7 @@ import avatar4 from "./src/assets/avatar4.svg";
 import imgModalSaldoCair from "./src/assets/imgModalSaldoCair.png";
 import empetyChat from "./src/assets/empetyChat.png";
 import imgNotFound404 from "./src/assets/imgNotFound404.png";
-import gambar from "./src/assets/gambar.png";
+import gambar from "./src/assets/Gambar.png";
 import accountBalance from "./src/assets/account-balance.png";
 import acountCircle from "./src/assets/account-circle.png";
 import localLibrary from "./src/assets/local-ibrary.png";
