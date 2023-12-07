79da7426179cf36c07e14d97dd85331d733587ee
karimDev <kromosom06@gmail.com>
2023-12-07 17:13:03 +0700

    first commit

---
+++ .vite/deps_temp_0ffce5dc/react-bootstrap_Modal.js
@@ -0,0 +1,15 @@
+"use client";
+import {
+  Modal_default2 as Modal_default
+} from "./chunk-X6I6MDBZ.js";
+import "./chunk-KVCEMKSC.js";
+import "./chunk-PYMATGNY.js";
+import "./chunk-4GQWNQSM.js";
+import "./chunk-HOFZDJTO.js";
+import "./chunk-MKG3NT3Y.js";
+import "./chunk-67XTWVEJ.js";
+import "./chunk-5WWUZCGV.js";
+export {
+  Modal_default as default
+};
+//# sourceMappingURL=react-bootstrap_Modal.js.map
