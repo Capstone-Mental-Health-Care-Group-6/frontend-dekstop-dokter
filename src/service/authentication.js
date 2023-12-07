f7afa7a4323ad8f88f889d38eec26ff603840bb0
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-12-07 11:36:23 +0700

    finish setup dokter

--- src/service/authentication.js
+++ src/service/authentication.js
@@ -32,3 +32,13 @@ export const forgetPassword = (formForgetPass, callback) => {
 };
 
 
+export const resetPassword = (id, formResetPass) => {
+    axios.post(`${url}/reset-password?token_reset_password=${id}`, formResetPass)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
