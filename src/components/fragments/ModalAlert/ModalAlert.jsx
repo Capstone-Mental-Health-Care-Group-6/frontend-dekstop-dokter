5626c83c4effc942d3c09cf24042a9c204886947
karimDev <kromosom06@gmail.com>
2023-12-18 11:37:02 +0700

    fixed icon alet in modal alert

--- src/components/fragments/ModalAlert/ModalAlert.jsx
+++ src/components/fragments/ModalAlert/ModalAlert.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import Button from "../../elements/Button/Button";
 
-function ModalAlert({ children, id, size }) {
+function ModalAlert({ children, id, iconAlert, size }) {
   return (
     <div
       className="modal fade"
@@ -10,7 +11,17 @@ function ModalAlert({ children, id, size }) {
       aria-hidden="true"
     >
       <div className={`modal-dialog modal-dialog-centered   ${size}`}>
-        <div className="modal-content">{children}</div>
+        <div className="modal-content">
+          <div className="border-0 d-flex justify-content-center position-relative">
+            <img src={iconAlert} alt="icon-alert" className="text-center" />
+            <Button
+              className={"btn-close position-absolute end-0"}
+              bsDismiss={"modal"}
+              ariaLabel={"Close"}
+            />
+          </div>
+          {children}
+        </div>
       </div>
     </div>
   );
